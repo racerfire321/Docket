c57169d0960f56aba764fe4a6b7ce50a51f8957b
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-08-10 22:50:00 +0545

    feat: add the localization in whole screen

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,4 @@
-import 'intl-pluralrules'; // Import the polyfill
+import 'intl-pluralrules'; 
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import * as Localization from 'react-native-localize';
@@ -6,22 +6,22 @@ import en from './locales/en.json';
 import fr from './locales/fr.json';
 import es from './locales/es.json';
 
-// Define the translations
+
 const resources = {
   en: { translation: en },
   fr: { translation: fr },
   es: { translation: es },
 };
 
-// Initialize i18n
+
 i18n
-  .use(initReactI18next) // Connects i18next with React
+  .use(initReactI18next) 
   .init({
-    resources, // Load translations
-    lng: Localization.getLocales()[0]?.languageTag || 'en', // Set the default language based on the device's locale
-    fallbackLng: 'en', // Fallback to English if the detected language is not supported
+    resources, 
+    lng: Localization.getLocales()[0]?.languageTag || 'en', 
+    fallbackLng: 'en', 
     interpolation: {
-      escapeValue: false, // React already escapes by default
+      escapeValue: false, 
     },
   });
 
